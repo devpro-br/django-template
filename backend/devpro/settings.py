23aa84a958352c38a05b980e45db48d80af3a852
renzo <renzon@gmail.com>
2024-07-11 21:37:08 -0300

    Inserida biblioteca dj_database_url para leitura de url de banco de dados
    close #20

--- backend/devpro/settings.py
+++ backend/devpro/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,14 +76,11 @@ WSGI_APPLICATION = 'devpro.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
+    "default": dj_database_url.parse(
+        'postgres://postgres:postgres@localhost:5432/postgres',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # Password validation
