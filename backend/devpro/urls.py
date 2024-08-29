985a7d087ae68148b7e0bd9daf1ad31e36cc9072
renzo <renzon@gmail.com>
2024-08-28 21:44:07 -0300

    Implementado fluxo com templates para login

--- backend/devpro/urls.py
+++ backend/devpro/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('devpro.base.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 if settings.AWS_STORAGE_BUCKET_NAME == '':
