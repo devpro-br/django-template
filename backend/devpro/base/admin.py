4237144803e31acff4ed0bb11f75878e3451584b
renzo <renzon@gmail.com>
2024-09-05 12:51:33 -0300

    Extraida lib para gestão de arquivos no s3 para lib devpro-s3-storages
    close #32

--- backend/devpro/base/admin.py
+++ backend/devpro/base/admin.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
-from django.contrib.admin import ModelAdmin
 from django_min_custom_user.admin import MinUserAdmin
 
-from devpro.base.models import User, Arquivo
+from devpro.base.models import User
 
 
 @admin.register(User)
 class UserAdmin(MinUserAdmin):
-    pass
-
-
-@admin.register(Arquivo)
-class ArquivoAdmin(ModelAdmin):
     pass
