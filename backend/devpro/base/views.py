baaa2a8dba154f95ba02b40a40b9999ac34a71af
renzo <renzon@gmail.com>
2024-10-02 20:09:07 -0300

    Incluido Middleware de login obritório para URLS
    close #42

--- backend/devpro/base/views.py
+++ backend/devpro/base/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_not_required
 from django.shortcuts import render
 
 
 # Create your views here.
 
+@login_not_required
 def home(request):
     return render(request, 'base/home.html')
